90411cd576dc7867b078b7329f6ea03f2089182c
sheikhnaved <75406847+sheikhnaved135@users.noreply.github.com>
2024-05-08 12:17:38 +0530

    Update Footer.jsx

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <>
       <div className="bg-dark text-white p-3">
         {" "}
-        <h3 className="text-center">All right reserved &copy;</h3>
+        <h3 className="text-center">All right reserved &copy; One1Store</h3>
         <p className="text-center">
           <Link to={"/about"}>About</Link>
           <Link to={"/contact"}>Contact</Link>
